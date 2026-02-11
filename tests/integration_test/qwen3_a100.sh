c6881d4302e834ad7ebd4206e256e5dd1ce06967
tianlef <1095012807@qq.com>
2026-02-11 21:23:40 +0800

    [Fleet CI release]Add new a100 release (#3884)

--- tests/integration_test/qwen3_a100.sh
+++ tests/integration_test/qwen3_a100.sh
@@ -1,71 +1,96 @@
 # Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
-#
+# 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
+# 
 #     http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 set -exo pipefail
 export root_dir=$(pwd)
+
+step=$1
+
+if [[ ! -d $CACHE_DIR/Qwen3-30B-A3B ]]; then
+    pushd $CACHE_DIR
+    wget -q --tries=5 --no-proxy https://xly-devops.cdn.bcebos.com/PaddleFleet/Qwen/Qwen3-30B-A3B.tar.gz --no-check-certificate
+    tar xf Qwen3-30B-A3B.tar.gz
+    popd
+fi
 
 if [ -f 'PaddleFleet/.venv/bin/activate' ]; then
    source PaddleFleet/.venv/bin/activate
 fi
 
-cd $root_dir/glm45_fleet
-export cur_dir=$(pwd)
+if [[ "$step" == "pt" ]]; then
+    export config_yaml=$root_dir/PaddleFormers/tests/config/ci/qwen3_multicard_pt.yaml
+    export data_dir=$root_dir/PaddleFormers/tests/fixtures/dummy/pt
+    export model_name_or_path=$CACHE_DIR/Qwen3-30B-A3B
+    export output_dir=$root_dir/checkpoints/qwen-pt
+    yq eval '.moe_grouped_gemm = false
+          | .stage1_overlap = false'\
+    $config_yaml > ${config_yaml}.tmp
+    mv ${config_yaml}.tmp $config_yaml
+elif [[ "$step" == "sft" ]]; then
+    export config_yaml=$root_dir/PaddleFormers/tests/config/ci/qwen3_multicard_sft.yaml
+    export data_dir=$root_dir/PaddleFormers/tests/fixtures/dummy/sft
+    export model_name_or_path=$root_dir/checkpoints/qwen-pt
+    export output_dir=$root_dir/checkpoints/qwen-sft
+    yq eval '.moe_grouped_gemm = false
+      | .stage1_overlap = false' \
+    $config_yaml > ${config_yaml}.tmp
+    mv ${config_yaml}.tmp $config_yaml
+else
+    export config_yaml=$root_dir/PaddleFormers/tests/config/ci/qwen3_multicard_lora.yaml
+    export data_dir=$root_dir/PaddleFormers/tests/fixtures/dummy/sft
+    export model_name_or_path=$root_dir/checkpoints/qwen-sft
+    export output_dir=$root_dir/checkpoints/qwen-lora
+fi
 
-# prepare dpo data
-export data_dir=$root_dir/PaddleFormers/tests/fixtures/dummy/dpo
-
-config_dpo_yaml=$root_dir/PaddleFormers/tests/config/ci/glm45_dpo_lora.yaml
-
-config_json=$CACHE_DIR/glm45/GLM-4.5-Air/config.json
-
-yq '.train_dataset_path = strenv(data_dir) + "/train.jsonl"
+yq eval '.train_dataset_path = strenv(data_dir) + "/train.jsonl"
     | .eval_dataset_path = strenv(data_dir) + "/eval.jsonl"
-    | .model_name_or_path = strenv(CACHE_DIR) + "/zai-org/GLM-4.5-Air-Base"
-    | .logging_dir = strenv(cur_dir) + "/glm_full_dpo_lora_vdl_log"
-    | .output_dir = strenv(cur_dir) + "/checkpoints/glm_full_dpo_lora_ckpts"' \
-   $config_dpo_yaml > ${config_dpo_yaml}.tmp
-mv ${config_dpo_yaml}.tmp $config_dpo_yaml
+    | .model_name_or_path = strenv(model_name_or_path)
+    | .use_expert_parallel = false
+    | .expert_model_parallel_size = 1
+    | .output_dir = strenv(output_dir)' \
+   $config_yaml > ${config_yaml}.tmp
+mv ${config_yaml}.tmp $config_yaml
+
 
 rm -rf ./outputs
 rm -rf paddleformers_dist_log
 master=$(hostname -i)
 port=36677
 
+export FLAGS_embedding_deterministic=1
+export FLAGS_cudnn_deterministic=1
 export FLAGS_use_stride_compute_kernel=False
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 unset http_proxy https_proxy
 
-export FLAGS_embedding_deterministic=1
-export FLAGS_cudnn_deterministic=1
-
-log_file=glm45_dpo_lora.txt
-gt_loss_file=glm45_dpo_lora_multi_card_gt_loss.txt
+log_file=qwen_${step}_multi_card_a100.txt
+gt_loss_file=qwen_${step}_multi_card_a100_gt_loss.txt
 
 set +e
-NNODES=1 MASTER_ADDR=$master MASTER_PORT=$port coverage run $(which paddleformers-cli) train $config_dpo_yaml 2>&1 | tee ./${log_file}
-sft_exit_code=$?
-if [ $sft_exit_code -ne 0 ]; then
-   echo "GLM4.5 multi-cards training failed, try to check the log file"
-   python $root_dir/PaddleFormers/tests/check_log_for_exitcode.py ./${log_file} "***** eval metrics *****"
-   sft_check_exit_code=$?
-   if [ $sft_check_exit_code -ne 0 ]; then
+NNODES=1 MASTER_ADDR=$master MASTER_PORT=$port coverage run $(which paddleformers-cli) train $config_yaml 2>&1 | tee ./${log_file}
+
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+   echo "qwen multi-cards training failed, try to check the log file"
+   python $root_dir/PaddleFormers/tests/check_log_for_exitcode.py ./${log_file} "***** train metrics *****"
+   check_exit_code=$?
+   if [ $check_exit_code -ne 0 ]; then
      echo "Failed to find 'Training completed' in log file."
      exit 1
    else
      echo "Log check passed."
    fi
+else
+    echo "Test passed."
 fi
 
 # export repo_name=$(echo $GITHUB_REPO_NAME | awk -F'/' '{print $2}')
