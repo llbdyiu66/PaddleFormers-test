5cc352e43c8a7ee9044da955d4d999b8fe246724
tianlef <1095012807@qq.com>
2026-02-28 22:19:36 +0800

    [fleet ci dev]change version (#3955)

--- setup.py
+++ setup.py
@@ -204,7 +204,7 @@ try:
         install_requires=REQUIRED_PACKAGES,
         entry_points={"console_scripts": get_console_scripts()},
         extras_require={
-            "paddlefleet": ["paddlefleet==0.2.0.post20260213+ff7891ef795"],
+            "paddlefleet": ["paddlefleet==0.2.0.post20260228+8519ae362f1"],
         },
         python_requires=">=3.8",
         classifiers=[
